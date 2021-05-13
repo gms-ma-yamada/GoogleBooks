c748e12b076468f86e6fc17dce4244710a14942e
ma-yamada <ma-yamada@global-mobility-service.com>
2021-05-13 14:45:05 +0900

    feat:edit delete index

--- bookapp/src/global/Header.vue
+++ bookapp/src/global/Header.vue
@@ -4,12 +4,19 @@
       app
       color="primary" dark>
       <v-spacer></v-spacer>
-    </v-app-bar></div>
+      <v-btn color="error" @click="deleteLocalStorage">削除する</v-btn>
+    </v-app-bar>
+    </div>
 </template>
 
 <script>
 export default {
-
+  name:'Header',
+  methods:{
+    deleteLocalStorage(){
+      this.$emit('delete-local-storage')
+    }
+  }
 }
 </script>
 
